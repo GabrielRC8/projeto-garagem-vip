4073ec5b5ce495e1a8584f60c716ef30c9ea1d15
Gabriel Rodrigues <gabrielrodrigues@localhost.localdomain>
2020-07-08 16:56:52 -0300

    pendencia

---
+++ app/Http/Controllers/Orders/OrdersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\Orders;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class OrdersController extends Controller
+{
+    public function index()
+    {
+
+        return view('Site.orders');
+
+
+
+
+
+    }
+}
